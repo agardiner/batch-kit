ded107d67b6a0768493c79c865071db694304a6f
Adam Gardiner <adam.a.gardiner@rbs.co.uk>
2014-11-27 15:06:31 +0000

    Add support for adding arguments to a class

---
+++ lib/batch/arguments.rb
@@ -0,0 +1,50 @@
+require 'arg_parser'
+require 'color-console'
+
+
+class Batch
+
+    # Defines a module for adding argument parsing to a job via the arg-parser
+    # gem, and displaying help via the color-console gem.
+    module Arguments
+
+        include ArgParser::DSL
+
+
+        # Adds an arguments accessor that returns the results from parsing the
+        # command-line.
+        attr_reader :arguments
+
+
+        # Parse command-line arguments
+        def parse_arguments(args = ARGV)
+            args_def.title ||= self.job_name.titleize
+            args_def.purpose ||= self.job_desc
+            arg_parser = ArgParser::Parser.new(args_def)
+            @arguments = arg_parser.parse(args)
+            if @arguments == false
+                if arg_parser.show_help?
+                    args_def.show_help(nil, Console.width || 80).each do |line|
+                        Console.puts line, :cyan
+                    end
+                    exit
+                else
+                    arg_parser.errors.each{ |error| log.error error }
+                    args_def.show_usage(nil, Console.width || 80).each do |line|
+                        Console.puts line, :yellow
+                    end
+                    exit(99)
+                end
+            end
+            @arguments
+        end
+
+
+        # Add class methods when module is included
+        def self.included(base)
+            base.extend(ClassMethods)
+        end
+
+    end
+
+end
---
+++ test/test_arguments.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require 'batch/arguments'
+
+class TestArguments < Test::Unit::TestCase
+
+    include Batch::Arguments
+
+    positional_arg :pos_arg, 'A positional arg'
+    keyword_arg :kw_arg, 'A keyword arg'
+    usage_break 'FLAGS'
+    flag_arg :flag1, 'Flag arg 1'
+
+
+    def test_parse
+        args_def.title = 'Test Arguments'
+        args_def.purpose = 'Test that parsing can be added to any class'
+        args = self.parse_arguments(['--kw_arg', 'kw_val', 'MyPos', '--flag1'])
+        assert_equal('MyPos', args.pos_arg)
+        # Parsed args should also be available via self#arguments
+        assert_equal('kw_val', arguments.kw_arg)
+        assert_equal(true, arguments.flag1)
+    end
+
+end
