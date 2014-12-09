4f3565793f2e90949f41c00cf917bdb99bb96394
Adam Gardiner <adam.b.gardiner@gmail.com>
2014-12-09 17:37:03 +0000

    Add a Batch::Configurable module for adding configurability to a class

--- lib/batch/config.rb
+++ lib/batch/config.rb
@@ -390,5 +390,45 @@ class Batch
 
     end
 
+
+    # Adds a configure class method that can be used to load a configuration file
+    # and make it available to the class and instances of it.
+    module Configurable
+
+        module ClassMethods
+
+            def configure(cfg_file = nil, options = {})
+                if cfg_file.is_a?(Hash)
+                    options = cfg_file
+                    cfg_file = nil
+                end
+                config.decryption_key = options[:decryption_key] if options[:decryption_key]
+                if cfg_file
+                    config.load(cfg_file, !options.fetch(:ignore_unknown_placeholders, true))
+                end
+                config
+            end
+
+
+            def config
+                @config ||= Batch::Config.new
+            end
+
+        end
+
+
+        def self.included(base)
+            base.extend(ClassMethods)
+        end
+
+
+        # Each object instance gets its own copy of the class configuration, so
+        # that any modifications they make are local to the object instance.
+        def config
+            @config ||= self.class.config.clone
+        end
+
+    end
+
 end
 
--- test/test_config.rb
+++ test/test_config.rb
@@ -76,4 +76,26 @@ class TestConfig < Test::Unit::TestCase
         assert_equal('foo', cfg.secret.password)
     end
 
+
+    class ConfigA
+        include Batch::Configurable
+    end
+
+
+    def test_configurable
+        assert(ConfigA.respond_to?(:configure))
+        assert_equal(Batch::Config, ConfigA.config.class)
+        assert_equal(0, ConfigA.config.size)
+        ConfigA.config.foo = 'bar'
+        assert_equal('bar', ConfigA.config.foo)
+        cfg_a = ConfigA.new
+        assert(cfg_a.respond_to?(:config))
+        assert_equal('bar', cfg_a.config.foo)
+        cfg_a.config.foo = 'baz'
+        assert_equal('baz', cfg_a.config.foo)
+        assert_equal('bar', ConfigA.config.foo)
+        ConfigA.configure("#{DIR}/test_config.yaml")
+        assert_equal('FooBar', ConfigA.config.prop2.prop2b)
+    end
+
 end
