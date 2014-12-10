fc7150b02e137f51662a7642319fdd89066d0536
Adam Gardiner <adam.b.gardiner@gmail.com>
2014-12-10 10:29:30 +0000

    Add support for testing against Oracle using Ruby OCI8

--- test/test_schema.rb
+++ test/test_schema.rb
@@ -1,14 +1,19 @@
 require 'test/unit'
 require 'batch/database/schema'
-require 'java'
-require 'C:/oracle/product/11.2.0/dbhome_1/jdbc/lib/ojdbc6.jar'
 
 
 class TestSchema < Test::Unit::TestCase
 
     def setup
         @schema = Batch::Database::Schema.new
-        @schema.connect('jdbc:oracle:thin:BATCH/b4tch@localhost:1521:ORCL')
+        if RUBY_ENGINE == 'java'
+            require 'java'
+            require 'C:/oracle/product/11.2.0/dbhome_1/jdbc/lib/ojdbc6.jar'
+            @schema.connect('jdbc:oracle:thin:BATCH/b4tch@localhost:1521:ORCL')
+        else
+            require 'oci8'
+            @schema.connect(adapter: 'oracle', user: 'BATCH', password: 'b4tch')
+        end
     end
 
     def test_create_schema
