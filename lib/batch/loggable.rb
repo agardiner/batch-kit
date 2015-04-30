4068fd7e43371f7263ef4d99b09bb34e13040deb
Adam Gardiner <adam.b.gardiner@gmail.com>
2015-04-30 14:33:06 +0100

    Further simplify Loggable

--- lib/batch/loggable.rb
+++ lib/batch/loggable.rb
@@ -13,7 +13,7 @@ class Batch
         end
 
 
-        def log_execute(run, proc_obj, *args)
+        Batch::Events.subscribe(Batch::Runnable, 'execute') do |run, proc_obj, *args|
             case run
             when Job::Run
                 proc_obj.log.info "Job '#{run.label}' started on #{run.computer} by #{run.run_by}"
@@ -21,18 +21,12 @@ class Batch
                 proc_obj.log.info "#{run.class.name.split('::')[-2]} '#{run.label}' started"
             end
         end
-        module_function :log_execute
 
 
-        def log_post_execute(run, proc_obj, ok)
+        Batch::Events.subscribe(Batch::Runnable, 'post-execute') do |run, proc_obj, ok|
             proc_obj.log.info "#{run.class.name.split('::')[-2]} '#{run.label}' completed #{
                     ok ? 'successfully' : 'with errors'} in #{run.elapsed} seconds"
         end
-        module_function :log_post_execute
-
-
-        Batch::Events.subscribe(Batch::Runnable, 'execute'){ |*args| log_execute(*args) }
-        Batch::Events.subscribe(Batch::Runnable, 'post-execute'){ |*args| log_post_execute(*args) }
 
     end
 
