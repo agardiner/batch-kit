4d4c6fe3ecaf53a4784e4a337c5fbda4c0c1f27a
Adam Gardiner <adam.b.gardiner@gmail.com>
2015-05-07 13:46:52 +0100

    Complete implementation of Batch::Lockable

--- lib/batch/lockable.rb
+++ lib/batch/lockable.rb
@@ -1,3 +1,7 @@
+require 'timeout'
+require_relative 'events'
+
+
 class Batch
 
     # Defines lockable behaviour, which can be added to any batch process.
@@ -14,19 +18,115 @@ class Batch
     # and processing will not take place.
     module Lockable
 
-        def around_execute(run)
-            #LockManager.lock(run, lock, lock_timeout, lock_wait_timeout)
+        # Attempts to obtain the named lock +lock_name+. If the lock is already
+        # held by another process, this method blocks until one of the following
+        # occurs:
+        # - the lock is released by the process that currently holds it
+        # - the lock expires, by reaching it's timeout period
+        # - the +lock_wait_timeout+ period is reached.
+        #
+        # Lock management is managed via the event publishing system; subscribers
+        # to the 'lock?' event indicate whether a lock is available by their
+        # response to the event. A value of false indicates the lock is
+        # currently held; a response of true indicates the lock has been granted.
+        #
+        # @param lock_name [String] The name of the lock that is needed.
+        # @param lock_timeout [Fixnum] The maximum number of seconds that this
+        #   process can hold the requested lock before it times out (allowing
+        #   any other processes waiting on the lock to proceed). This value
+        #   should be set high enough that the lock does not timeout while
+        #   processing that relies on the lock is not still running.
+        # @param lock_wait_timeout [Fixnum] The maximum time this process is
+        #   prepared to wait for the lock to become available. If not specified,
+        #   the wait will timeout after the same amount of time as +lock_timeout+.
+        # @raise Timeout::Error If the lock is not obtained within
+        #   +lock_wait_timeout+ seconds.
+        def lock(lock_name, lock_timeout, lock_wait_timeout = nil)
+            unless lock_timeout && lock_timeout.is_a?(Fixnum) && lock_timeout > 0
+                raise ArgumentError, "Invalid lock_timeout; must be > 0"
+            end
+            unless lock_wait_timeout.nil? || (lock_wait_timeout.is_a?(Fixnum) && lock_wait_timeout > 0)
+                raise ArgumentError, "Invalid lock_wait_timeout; must be nil or > 0"
+            end
+            unless Batch::Events.has_subscribers?(self, 'lock?')
+                if self.respond_to?(:log)
+                    log.detail "No lock manager available; proceeding without locking"
+                end
+                return
+            end
+            lock_wait_timeout ||= lock_timeout
+            lock_expire_time = nil
+            wait_expire_time = Time.now + lock_wait_timeout
             begin
-                super
-            ensure
-                #LockManager.unlock(run, lock)
+                Timeout.timeout(lock_wait_timeout) do
+                    i = 0
+                    while Time.now < wait_expire_time do
+                        lock_expire_time = Batch::Events.publish(self, 'lock?', lock_name, lock_timeout)
+                        break if lock_expire_time
+                        if i == 0
+                            Batch::Events.publish(self, 'lock_wait', lock_name, wait_expire_time)
+                            if self.respond_to?(:log)
+                                log.detail "Waiting for lock '#{lock_name}' to become avaialable"
+                            end
+                        end
+                        sleep 1
+                        i += 1
+                    end
+                    Batch::Events.publish(self, 'locked', lock_name, lock_expire_time)
+                    if self.respond_to?(:log)
+                        log.detail "Obtained lock '#{lock_name}'; expires at #{
+                            lock_expire_time.strftime('%H:%M:%S')}"
+                    end
+                end
+            rescue Timeout::Error
+                Batch::Events.publish(self, 'lock_wait_timeout', lock_name, wait_expire_time)
+                if self.respond_to?(:log)
+                    log.error "Timed out waiting for lock '#{lock_name}' to become available"
+                end
+                raise
             end
         end
 
 
-        def self.included(cls)
-            # Add lock properties
-            # Alias method chain around_execute
+        # Release a lock held by this object.
+        #
+        # @param lock_name [String] The name of the lock to be released.
+        def unlock(lock_name)
+            unless Batch::Events.has_subscribers?(self, 'unlock')
+                return
+            end
+            resp = Batch::Events.publish(self, 'unlock', lock_name)
+            if self.respond_to?(:log)
+                if resp
+                    log.detail "Released lock '#{lock_name}'"
+                else
+                    log.warn "Unlock failed - lock not held; check lock timeout is sufficient"
+                end
+            end
+        end
+
+
+        # Obtains the requested +lock_name+, then yields to the supplied block.
+        # Ensures the lock is released when the block ends or raises an error.
+        #
+        # @param lock_name [String] The name of the lock to obtain.
+        # @param lock_timeout [Fixnum] The maximum number of seconds that this
+        #   process can hold the requested lock before it times out (allowing
+        #   any other processes waiting on the lock to proceed). This value
+        #   should be set high enough that the lock does not timeout while
+        #   processing that relies on the lock is not still running.
+        # @param lock_wait_timeout [Fixnum] The maximum time this process is
+        #   prepared to wait for the lock to become available. If not specified,
+        #   the wait will timeout after the same amount of time as +lock_timeout+.
+        # @raise Timeout::Error If the lock is not obtained within
+        #   +lock_wait_timeout+ seconds.
+        def with_lock(lock_name, lock_timeout, lock_wait_timeout = nil)
+            self.lock(lock_name, lock_timeout, lock_wait_timeout)
+            begin
+                yield
+            ensure
+                self.unlock(lock_name)
+            end
         end
 
     end
---
+++ test/test_locking.rb
@@ -0,0 +1,82 @@
+require 'test/unit'
+require 'batch/lockable'
+
+
+
+class TestLocking < Test::Unit::TestCase
+
+    include Batch::Lockable
+
+
+    def setup
+        @locks = {}
+        Batch::Events.subscribe(self, 'lock?') do |requestor, lock_name, lock_timeout|
+            puts "Lock request received for #{lock_name}"
+            if @locks[lock_name] == nil || @locks[lock_name][:expires] < Time.now
+                @locks[lock_name] = {holder: requestor, expires: Time.now + lock_timeout}
+            end
+        end
+        Batch::Events.subscribe(self, 'unlock') do |requestor, lock_name|
+            assert_equal(requestor, @locks[lock_name][:holder])
+            @locks.delete(lock_name)
+        end
+    end
+
+
+    def teardown
+        Batch::Events.unsubscribe(self, 'lock?')
+        Batch::Events.unsubscribe(self, 'unlock')
+    end
+
+
+    def test_no_lock_manager
+        teardown
+        lock('test_no_lock_manager', 3)
+        assert_equal(0, @locks.size)
+        ran = false
+        with_lock('test_with_lock_no_lock_manager', 3) do
+            ran = true
+        end
+        assert(ran)
+        unlock('test_no_lock_manager')
+    end
+
+
+    def test_lock_when_available
+        lock('test_lock', 3)
+        assert_equal(1, @locks.size)
+        unlock('test_lock')
+        assert_equal(0, @locks.size)
+    end
+
+
+    def test_wait_timeout
+        lock('test_wait_timeout', 5)
+        assert_raises(Timeout::Error) do
+            lock('test_wait_timeout', 3, 1)
+        end
+    end
+
+
+    def test_lock_timeout
+        lock('test_lock_timeout', 1)
+        sleep 2
+        lock('test_lock_timeout', 5)
+        assert_equal(1, @locks.size)
+    end
+
+
+    def test_with_lock
+        ran = false
+        assert_raises(ArgumentError) do
+            with_lock('with_lock', 5) do
+                ran = true
+                assert_equal(1, @locks.size)
+                raise ArgumentError
+            end
+        end
+        assert(ran)
+        assert_equal(0, @locks.size)
+    end
+
+end
