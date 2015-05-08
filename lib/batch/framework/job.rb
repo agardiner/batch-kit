20e2db5b6ef59cfc11cf45a68f85eb28bb2504e6
Adam Gardiner <adam.b.gardiner@gmail.com>
2015-05-08 14:04:11 +0100

    Add #with_lock convenience method to Batch::Job

--- lib/batch/framework/job.rb
+++ lib/batch/framework/job.rb
@@ -1,7 +1,6 @@
 require 'batch/arguments'
 require 'batch/configurable'
 require 'batch/loggable'
-require 'batch/lockable'
 
 
 # Default log level is :detail
@@ -15,7 +14,6 @@ class Batch
         include Arguments
         include Configurable
         include Loggable
-        include Lockable
 
 
         # Include ActsAsJob into any inheriting class
@@ -33,6 +31,19 @@ class Batch
             job = self.new
             job.parse_arguments
             job.send(self.job.method_name)
+        end
+
+
+        # Convenience method for using a lock within a job method
+        #
+        # @param lock_name [String] The name of the lock to obtain during
+        #   execution of the block.
+        # @param lock_timeout [Fixnum] The maximum time (in seconds) until the
+        #   lock should expire.
+        # @param wait_timeout [Fixnum] An optional time (in seconds) to wait for
+        #   the lock to become available if it is already in use.
+        def with_lock(lock_name, lock_timeout, wait_timeout = nil, &blk)
+            self.job_run.with_lock(lock_name, lock_timeout, wait_timeout, &blk)
         end
 
     end
