5f94464aa04e5be0802723132b6f412c850b3091
Adam Gardiner <adam.b.gardiner@gmail.com>
2015-09-21 23:58:59 +0100

    Move Batch::Process into Helpers namespace

--- test/test_process.rb
+++ test/test_process.rb
@@ -1,7 +1,7 @@
-require 'test/unit'
-require 'batch/process'
+require 'minitest/autorun'
+require 'batch/helpers/process'
 
-class TestProcess < Test::Unit::TestCase
+class TestProcess < Minitest::Test
 
 
     def test_popen
@@ -9,7 +9,7 @@ class TestProcess < Test::Unit::TestCase
 
 
     def test_launch
-        Batch::Process.launch('cmd.exe /C dir', log_level: :info)
+        Batch::Helpers::Process.launch('cmd.exe /C dir', log_level: :info)
     end
 
 end
