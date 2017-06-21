6eb979d06d1caaa7896411b6c008470a32a68c6e
Adam Gardiner <adam.b.gardiner@gmail.com>
2017-06-21 11:32:49 +0100

    Fix issue with aliasing File#open to add BOM support

--- lib/batch/core_ext/file.rb
+++ lib/batch/core_ext/file.rb
@@ -9,43 +9,6 @@ class Batch
                 File.basename(path, File.extname(path))
             end
 
-
-            unless defined?(:open_without_bom)
-
-                # Add support for writing a BOM if the +mode_string+ includes 'bom',
-                # and the mode is write.
-                alias_method :open_without_bom, :open
-                def open(*args, &blk)
-                    if args.length >= 2 && (mode_string = args[1]).is_a?(String) &&
-                        mode_string =~ /^(w|a):(.*)bom/i
-                        args[1] = mode_string.sub(/bom\||[\-|]bom/, '')
-                        f = open_without_bom(*args)
-                        bom_hex = case mode_string
-                        when /utf.*8/i
-                            "\xEF\xBB\xBF"
-                        when /utf-16be/i
-                            "\xFE\xFF"
-                        when /utf-16le/i
-                            "\xFF\xFE"
-                        when /utf-32be/i
-                            "\x00\x00\xFE\xFF"
-                        when /utf-32le/i
-                            "\xFE\xFF\x00\x00"
-                        end
-                        f << bom_hex.force_encoding(f.external_encoding)
-                        if block_given?
-                            yield f
-                            f.close
-                        else
-                            f
-                        end
-                    else
-                        open_without_bom(*args, &blk)
-                    end
-                end
-
-            end
-
         end
 
 
@@ -59,5 +22,47 @@ end
 
 
 File.class_eval do
+
     include Batch::FileExtensions
+
+    class << self
+
+        unless method_defined?(:open_without_bom)
+
+            # Add support for writing a BOM if the +mode_string+ includes 'bom',
+            # and the mode is write.
+            alias_method :open_without_bom, :open
+            def open(*args, &blk)
+                if args.length >= 2 && (mode_string = args[1]).is_a?(String) &&
+                    mode_string =~ /^(w|a):(.*)bom/i
+                    args[1] = mode_string.sub(/bom\||[\-|]bom/, '')
+                    f = open_without_bom(*args)
+                    bom_hex = case mode_string
+                    when /utf-?8/i
+                        "\xEF\xBB\xBF"
+                    when /utf-16be/i
+                        "\xFE\xFF"
+                    when /utf-16le/i
+                        "\xFF\xFE"
+                    when /utf-32be/i
+                        "\x00\x00\xFE\xFF"
+                    when /utf-32le/i
+                        "\xFE\xFF\x00\x00"
+                    end
+                    f << bom_hex.force_encoding(f.external_encoding)
+                    if block_given?
+                        yield f
+                        f.close
+                    else
+                        f
+                    end
+                else
+                    open_without_bom(*args, &blk)
+                end
+            end
+
+        end
+
+    end
+
 end
