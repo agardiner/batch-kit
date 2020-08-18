a5db0bde8d4df2458dadbc52c6df1b1c24137bba
Adam Gardiner <adam.b.gardiner@gmail.com>
2020-08-18 10:00:28 +0100

    Fix Zip helper for changes in zip gem

--- lib/batch-kit/helpers/zip.rb
+++ lib/batch-kit/helpers/zip.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
-require 'zip/zip'
+require 'zip'
 
 
 class BatchKit
@@ -15,8 +15,8 @@ class BatchKit
             #   the zip.
             def create_zip(zip_file, *files)
                 FileUtils.rm_f(zip_file)
-                Zip::ZipFile.open(zip_file, Zip::ZipFile::CREATE) do |zip|
-                    files.each do |file|
+                ::Zip::File.open(zip_file, ::Zip::File::CREATE) do |zip|
+                    files.flatten.each do |file|
                         zip.add(File.basename(file), file)
                     end
                     yield zip if block_given?
