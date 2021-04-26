50985f7a7be7bb6b32da7ce1a2d7c03a5c949c17
Adam Gardiner <adam.b.gardiner@gmail.com>
2021-04-26 16:23:05 +0100

    Add an #unzip command to the zip helper

--- lib/batch-kit/helpers/zip.rb
+++ lib/batch-kit/helpers/zip.rb
@@ -23,6 +23,30 @@ class BatchKit
                 end
             end
 
+
+            # Unzip a +zip_flie+ to +output_dir+.
+            #
+            # @param zip_file [String] A path to the zip file to be created
+            # @param output_dir [String] A path to an output directory where
+            #   the zip content should be unzipped.
+            # @param file_spec [String] An optional file pattern of zip entries
+            #   to be unzipped. If not specified, all files are extracted.
+            def unzip(zip_file, output_dir, file_spec=nil)
+                paths = []
+                ::Zip::File.open(zip_file) do |zip|
+                    zip.each do |entry|
+                        if file_spec
+                            pn = Pathname.new(entry.name)
+                            next unless pn.fnmatch(file_spec)
+                        end
+                        path = File.join(output_dir, entry.name)
+                        entry.extract(path)
+                        paths << path
+                    end
+                end
+                paths
+            end
+
         end
 
     end
